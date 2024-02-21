d1fad824b8bf33b01ea4a7a0976dcaeaa3ec3d42
Avelia Diva Zahra <avelia.diva@ui.ac.id>
2024-02-21 16:03:58 +0700

    sound effect

--- scenes/main_level/ObjectiveArea.gd
+++ scenes/main_level/ObjectiveArea.gd
@@ -5,8 +5,10 @@ func _on_ObjectiveArea_body_entered(body: RigidBody2D):
 		var myNotify = get_node("MyLabel")
 		myNotify.text = "Level Completed! \n Loading Next Level..."
 		
+		$"../Success".play()
+		
 		var bunny = get_node("bunny")
 		bunny.visible = false
 		
-		yield(get_tree().create_timer(2), "timeout")
+		yield(get_tree().create_timer(1), "timeout")
 		get_tree().change_scene("res://scenes/second_level/SecondLevel.tscn")
--- scenes/second_level/ObjectiveArea2.gd
+++ scenes/second_level/ObjectiveArea2.gd
@@ -3,11 +3,14 @@ extends Area2D
 
 func _on_ObjectiveArea2_body_entered(body):
 	if (body.name == "GreenShip"):
+		
+		$"../Success".play()
+		
 		var ferret = get_node("ferret")
 		ferret.visible = false
 		
 		var myNotify = get_node("MyLabel")
 		myNotify.text = "Congratulations!\nYou Have Finished The Game!"
 		
-		yield(get_tree().create_timer(2), "timeout")
+		yield(get_tree().create_timer(1), "timeout")
 		get_tree().change_scene("res://scenes/end.tscn")
