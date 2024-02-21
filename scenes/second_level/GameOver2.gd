37ef77d819f617931757858163dd11113131c682
Avelia Diva Zahra <avelia.diva@ui.ac.id>
2024-02-21 16:30:27 +0700

    add meteor

--- scenes/second_level/GameOver2.gd
+++ scenes/second_level/GameOver2.gd
@@ -6,3 +6,7 @@ func _on_GameOver_body_entered(body: RigidBody2D):
 		$"../ObjectiveArea2/MyLabel".text = "You Failed!\nRestarting..."
 		yield(get_tree().create_timer(2), "timeout")
 		get_tree().change_scene("res://scenes/main_level/MainLevel.tscn")
+
+
+func _on_Meteor_body_entered(body: RigidBody2D):
+	get_tree().change_scene("res://scenes/second_level/SecondLevel.tscn")
