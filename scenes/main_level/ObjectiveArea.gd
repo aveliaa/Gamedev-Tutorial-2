f9d6396e44a14a0d1fc8157cfca8244a05c9e528
Avelia Diva Zahra <avelia.diva@ui.ac.id>
2024-02-21 12:50:39 +0700

    level transition, feedback

--- scenes/main_level/ObjectiveArea.gd
+++ scenes/main_level/ObjectiveArea.gd
@@ -2,4 +2,8 @@ extends Area2D
 
 func _on_ObjectiveArea_body_entered(body: RigidBody2D):
 	if (body.name == "BlueShip"):
-		print("Reached objective!")
+		var myNotify = get_node("MyLabel")
+		myNotify.text = "Level Completed! \n Loading Next Level..."
+		
+		yield(get_tree().create_timer(2), "timeout")
+		get_tree().change_scene("res://scenes/second_level/SecondLevel.tscn")
--- scenes/main_level/PlatformBlue.gd
+++ scenes/main_level/PlatformBlue.gd
@@ -1,10 +1,13 @@
 extends Node2D
 
+
 export var speed = 500.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	print("Platform initialized")
+
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
